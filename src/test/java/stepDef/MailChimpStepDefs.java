a38abbf5146a6387f7d2815f9825b9f1ae5a088d
mahmutalban <mahmut.alban@utb.ecutbildning.se>
2021-06-16 11:13:13 +0200

    removed old chrome exe file

--- src/test/java/stepDef/MailChimpStepDefs.java
+++ src/test/java/stepDef/MailChimpStepDefs.java
@@ -1,7 +1,5 @@
 package stepDef;
 
-import static org.junit.Assert.assertTrue;
-
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
@@ -12,8 +10,6 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -64,7 +60,10 @@ public class MailChimpStepDefs {
 		System.out.println("enter email");
 		//generate a random number and append it to what is sent via feature file
 		// this is to make data unique for reusibility
-		this.email = email.replace("@", new Random().nextInt(range)+"@");
+		if(!email.equalsIgnoreCase("test1@mail.com"))
+			this.email = email.replace("@", new Random().nextInt(range)+"@");
+		else 
+			this.email = email;
 		
 		//find email element and put above fabricated email
 		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(this.email);
@@ -75,7 +74,10 @@ public class MailChimpStepDefs {
 		System.out.println("enter userName");
 		//generate a random number and append it to what is sent via feature file
 		// this is to make data unique for reusibility
-		this.userName = new Random().nextInt(range) + ""+userName;
+		if(userName.equalsIgnoreCase("test1@mail.com"))
+			this.userName = userName;
+		else
+			this.userName = new Random().nextInt(range) + ""+userName;
 
 		//find email element and put above fabricated username
 		driver.findElement(By.xpath("//*[@id=\"new_username\"]")).sendKeys(this.userName);
@@ -89,70 +91,52 @@ public class MailChimpStepDefs {
 		driver.findElement(By.xpath("//*[@id=\"new_password\"]")).sendKeys(password);
 	}
 
-	@Then("Verify that signup is successfull")
-	public void verify_that_signup_is_successfull() throws AWTException {
+	@Then("Verify that signup process is {string}")
+	public void verify_that_signup_is(String response) throws AWTException{
 		System.out.println("Verify signup");
+		
 		pressTab();
 		pressEnter();
 		
-		//after successfull signup, get the email on success signup page
-	    String emailAfterSignup = driver.findElement(By.xpath("//*[@id=\"signup-content\"]/div/div/div/p/strong")).getText();
-	    
-	    //and compare it with what user entered at signup page, and if it matches, test case is passed
-	    Assert.assertEquals(emailAfterSignup, this.email);
-	    
-	}
+		if(response.equalsIgnoreCase("SUCCESS")) {
+			
+			//after successfull signup, get the email on success signup page
+		    String emailAfterSignup = driver.findElement(By.xpath("//*[@id=\"signup-content\"]/div/div/div/p/strong")).getText();
+		    
+		    //and compare it with what user entered at signup page, and if it matches, test case is passed
+		    Assert.assertEquals(emailAfterSignup, this.email);
+			
+		}
+		else if (response.equalsIgnoreCase("MAX_CHARACTERS_EXCEED")) {
+			
+			//get error message text while error on long userName
+		    String userMaxCharacterExceedErrorMessage = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span")).getText();
+		    
+		    //verify that error exist
+		    Assert.assertTrue(userMaxCharacterExceedErrorMessage.contains("Enter a value less than 100 characters"));
+		}
+		else if(response.equalsIgnoreCase("USER_ALREADY_EXIST")) {
+			System.out.println("alreadyyyyyy" + email);
+			//get error message text when user already exists
+		    String userAlreadyExistsErrorMessage = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span")).getText();
+		    
+		    //verify that valid error exists
+		    Assert.assertTrue(userAlreadyExistsErrorMessage.contains("username already exists"));
 
-	@When("I enter occupied userName as {string}")
-	public void i_enter_user_name(String userName) {
-		System.out.println("enter occupied userName");
-		
-		//find email element and put above username
-		driver.findElement(By.xpath("//*[@id=\"new_username\"]")).sendKeys(userName);
-	}
-
-	@Then("Verify that signup throws userName max 100 characters error")
-	public void verify_that_signup_throws_userName_max_100_characyers_error() throws AWTException {
-		System.out.println("Verify signup error");
-		pressTab();
-		pressEnter();
-		
-		//get error message text while error on long userName
-	    String userMaxCharacterExceedErrorMessage = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span")).getText();
+		}
+		else if(response.equalsIgnoreCase("MISSING_EMAIL")) {
+			//get error message text when email is missing
+		    String missingEmailErrorMessage = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span")).getText();
+		    System.err.println("ERRR>>" + missingEmailErrorMessage);
+		  
+		    //verify that valid error message is displayed
+		    Assert.assertTrue(missingEmailErrorMessage.contains("Please enter a value"));
+		    Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"av-flash-errors\"]/ul/li")).isDisplayed());
+		}
 	    
-	    //verify that error exist
-	    Assert.assertTrue(userMaxCharacterExceedErrorMessage.contains("Enter a value less than 100 characters"));
-	}
-
-	@Then("Verify that signup throws already exists error")
-	public void verify_that_signup_throws_user_already_exists_error() throws AWTException {
-		System.out.println("Verify signup error");
-		pressTab();
-		pressEnter();
-		
-		//get error message text when user already exists
-	    String userAlreadyExistsErrorMessage = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span")).getText();
+	    driver.quit();
 	    
-	    //verify that valid error exists
-	    Assert.assertTrue(userAlreadyExistsErrorMessage.contains("username already exists"));
 	}
-	
-	@Then("Verify that signup throws mising email error")
-	public void verify_that_signup_throws_missing_email_error() throws AWTException {
-		System.out.println("Verify signup missing email error");
-		pressTab();
-		pressEnter();
-		
-		//get error message text when email is missing
-	    String missingEmailErrorMessage = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span")).getText();
-	    System.err.println("ERRR>>" + missingEmailErrorMessage);
-	  
-	    //verify that valid error message is displayed
-	    Assert.assertTrue(missingEmailErrorMessage.contains("Please enter a value"));
-	    Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"av-flash-errors\"]/ul/li")).isDisplayed());
-
-	}
-	
 	
 	private void pressEnter() throws AWTException {
 		//using Java AWT, press Enter key while cursor is at current position
--- src/test/resources/features/mail-chimp-test.feature
+++ src/test/resources/features/mail-chimp-test.feature
@@ -3,39 +3,18 @@ Feature: Test MailChimp Website Signup Process
 	In order to verify the signp MailChimp Signup page works fine
 	We want to input *email*, *userName* and *Password* with different set of data 
 
-@VerifyThatSignupPageWorksWithValidData
-Scenario: To Verify that signup processes successfully with valid data
-	Given Open the Chrome Browser 
-	And Start Application
-	When I enter email as "test@mail.com"
-	And I enter userName as "test@mail.com"
-	And I enter password as "Pass@123#"
-	Then Verify that signup is successfull
-	
-@VerifyThatSignupPageWorksWithUserNameHavingMoreThan100Characters
-Scenario: To Verify that signup processes successfully with valid data
-	Given Open the Chrome Browser 
-	And Start Application
-	When I enter email as "test@mail.com"
-	And I enter userName as "testttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt@mail.com"
-	And I enter password as "Pass@123#"
-	Then Verify that signup throws userName max 100 characters error
 
-	
-@VerifyThatSignupPageThrowsErrorIfUserNameAlreadyOccupied
-Scenario: To Verify that signup processes successfully with valid data
+@VerifyThatSignupPageWorksWithDifferentData
+Scenario Outline: To Verify that signup processes with multiple input data
 	Given Open the Chrome Browser 
 	And Start Application
-	When I enter email as "test@mail.com"
-	And I enter occupied userName as "test@mail.com"
-	And I enter password as "Pass@123#"
-	Then Verify that signup throws already exists error
-	
-
-@VerifyThatSignupPageThrowsErrorIfEmailIsMissing
-Scenario: To Verify that signup processes successfully with valid data
-	Given Open the Chrome Browser 
-	And Start Application
-	When I enter userName as "test@mail.com"
-	And I enter password as "Pass@123#"
-	Then Verify that signup throws mising email error
+	When I enter email as <email>
+	And I enter userName as <userName>
+	And I enter password as <password>
+	Then Verify that signup process is <response>
+	Examples: lm
+		|	email	|	userName	|	password	|	response	|
+		|	"test@mail.com"	|	"test@mail.com"	|	"Pass@123#"	|	"SUCCESS"	|
+		|	"test@mail.com"	|	"testttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt@mail.com"	|	"Pass@123#"	|	"MAX_CHARACTERS_EXCEED"	|
+		|	"test1@mail.com"	|	"test1@mail.com"	|	"Pass@123#"	|	"USER_ALREADY_EXIST"	|
+		|	""	|	"test@mail.com"	|	"Pass@123#"	|	"MISSING_EMAIL"	|
